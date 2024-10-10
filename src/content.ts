2e15c334ee515f4927244561ea6e9407364b9755
Luca Engelhard <me@lucaengelhard.com>
2024-10-11 00:02:07 +0200

    comments & pappnasen & content file

---
+++ src/content.ts
@@ -0,0 +1,23 @@
+export const ananasCopyPasta = `## Beweis durch vollständige Induktion: Ananas auf Pizza ist schrecklich
+
+**Induktionsanfang:**  
+
+Wir betrachten den Fall n = 1, also eine Pizza mit einer einzigen Ananasscheibe. Die Süße der Ananas kollidiert mit dem herzhaften Geschmack des Käses und der Sauce. Das Ergebnis ist ein geschmackliches Chaos, das von den meisten Geschmacksnerven als unangenehm empfunden wird. 
+
+**Induktionsvoraussetzung:**  
+
+Angenommen, für ein beliebiges k ≥ 1 ist es wahr, dass eine Pizza mit k Ananasscheiben schrecklich schmeckt.
+
+**Induktionsschritt:**  
+
+Wir müssen zeigen, dass die Aussage auch für k + 1 Ananasscheiben gilt. 
+ 
+Wenn wir eine weitere Ananasscheibe zu einer Pizza mit k Ananasscheiben hinzufügen, verstärken wir den Effekt der Süße. Dies führt zu einer noch stärkeren Dissonanz mit den anderen Geschmacksrichtungen und verstärkt den Ekel des Geschmacks.  
+
+**Schlussfolgerung:**  
+
+Durch vollständige Induktion haben wir bewiesen, dass eine Pizza mit einer beliebigen Anzahl von Ananasscheiben schrecklich schmeckt.  
+`;
+
+export const ananasItaly =
+  "🇮🇹🤌🇮🇹🤌🇮🇹🤌 ANANAS AUF PIZZA IST EIN HASSVERBRECHEN 🇮🇹🤌🇮🇹🤌🇮🇹🤌";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { ananasCopyPasta, ananasItaly } from "./content";
 const token = process.env.DISCORD_TOKEN;
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY ?? "");
@@ -22,7 +23,15 @@ client.once(Events.ClientReady, (readyClient) => {
 
 client.login(token);
 
-// State
+/**
+ *
+ * STATE
+ *
+ * TODO: Schlafenszeiten des Bots (Meinte Alfi nicht, dass er zwischen 4 und 8 schläft? -> botreaktionen daktiviert oder fangen damit an, dass er eigentslich schon schlafen will)
+ *
+ */
+
+// Banger - Alle 10 Minuten besteht die Möglichkeit, dass als reaktion auf eine Nachricht Alfi sagt, wie banger die nachricht ist
 let bangerReady = true;
 
 setInterval(() => {
@@ -31,6 +40,7 @@ setInterval(() => {
   console.log("Banger is ready");
 }, 600000);
 
+//Message reactions
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
   pappnasen();
@@ -45,20 +55,26 @@ client.on("messageCreate", async (message) => {
   ) {
     const prompt = splitMessageString.slice(2).join(" ");
 
-    const result = await model.generateContent(
-      prompt + " die antwort sollte maximal 2000 zeichen lang sein"
-    );
+    try {
+      const result = await model.generateContent(
+        prompt + " die antwort sollte maximal 2000 zeichen lang sein"
+      );
+      message.channel.send(result.response.text().substring(0, 2000));
+    } catch (error) {
+      message.channel.send("Hey Sorry ich weiß auch nicht");
+    }
 
-    message.channel.send(result.response.text().substring(0, 2000));
     return;
   }
 
   // Common Reactions
   if (istAnanasAufPizza(messageString)) {
-    message.channel.send(
-      "🇮🇹🤌🇮🇹🤌🇮🇹🤌 ANANAS AUF PIZZA IST EIN HASSVERBRECHEN 🇮🇹🤌🇮🇹🤌🇮🇹🤌"
-    );
-    return;
+    if (Math.random() > 0.5) {
+      message.channel.send(ananasItaly);
+      return;
+    } else {
+      message.channel.send(ananasCopyPasta);
+    }
   }
 
   if (bangerReady && Math.random() > 0.5) {
@@ -76,14 +92,40 @@ function istAnanasAufPizza(text: string) {
   );
 }
 
+// Je nach reaktion schreibt er hier alle Stunde, dass ja viele Pappnasen da sind, oder das es echt wenige sind (maybe mit schlafenszeiten? damit nicht nachts die ganze zeit notifications kommen?)
+setInterval(() => {
+  pappnasen();
+}, 3600000);
+
 function pappnasen() {
   client.guilds.cache.forEach((guild) => {
-    const usersOnline = guild?.members.cache.filter(
-      (member) =>
-        member.presence &&
-        ["online", "idle", "dnd"].includes(member.presence.status)
+    const channels = guild.channels.cache.filter((channel) =>
+      ["nebenthemen", "allgemein", "general"].includes(channel.name)
     );
 
-    console.log(usersOnline?.map((user) => user.user.tag));
+    const channelNames = channels.map((channel) => channel.name);
+
+    const users = guild?.members.cache;
+
+    const usersNoBots = users.filter((member) => !member.user.bot);
+    const userNotBotsArray = usersNoBots.map((user) => user);
+
+    const usersOnline = usersNoBots.filter(
+      (member) =>
+        member.presence &&
+        ["online", "idle" /*"dnd"*/].includes(member.presence.status)
+    );
+
+    const usersOnlineArray = usersOnline.map((user) => user);
+    const sendChannel = channels.at(0);
+    if (usersOnlineArray.length >= userNotBotsArray.length / 2) {
+      if (sendChannel && sendChannel.isSendable()) {
+        sendChannel.send("Das sind ja ganz schön viele Pappnasen hier");
+      }
+    } else {
+      if (sendChannel && sendChannel.isSendable()) {
+        sendChannel.send("Wo sind die ganzen Pappnasen?");
+      }
+    }
   });
 }
