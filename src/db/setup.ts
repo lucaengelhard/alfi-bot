cfe93b856567d2b455990a7ef652280fc31c1673
Luca Engelhard <me@lucaengelhard.com>
2025-03-31 23:06:50 +0200

    connection string

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -4,23 +4,11 @@ import pg from "pg";
 
 const { Client: pgClientInit } = pg;
 export const pgclient = new pgClientInit({
-  user: env("PROD_DB_USER"),
-  password: env("PROD_DB_PASSWORD"),
-  host: env("PROD_DB_HOST"),
-  database: env("PROD_DB_NAME"),
+  connectionString: env("PROD_DB_CONNECTIONSTRING"),
   ssl: true,
 });
 
 await pgclient.connect();
-
-/* const res = await pgclient.query(`CREATE TABLE server(
-    id SERIAL PRIMARY KEY,
-    guild_name VARCHAR(255),
-    guild_id VARCHAR(255),
-    all_channels BOOLEAN,
-    allowed_channel_ids VARCHAR(255) [],
-    blocked_channel_ids VARCHAR(255) []
-)`); */
 
 const res = await pgclient.query("SELECT * FROM server");
 
